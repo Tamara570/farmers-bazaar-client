92796de980683c25a0461f5a9296be7c5dbffd4a
Tamara Walker <tamaralw115@gmail.com>
2020-11-14 14:40:28 -0500

    new tokenkey

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ let tokenKey;
 if (process.env.NODE_ENV === 'production') {
 
   apiPath = 'https://farmers-bazaar-server.herokuapp.com'
-  tokenKey = 'bazaar-dev-auth-token'
+  tokenKey = '3c02a47f10871c3fe78046a1f6c01a8ad0c3a997152736e3de5ae84baaf9cd2e'
   
 } else {
   apiPath = 'http://localhost:8000/api'
