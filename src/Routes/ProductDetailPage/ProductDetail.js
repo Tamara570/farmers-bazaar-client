18fd083a1bf28469f5f697789e78844e2ab3eb4d
Tamara Walker <tamaralw115@gmail.com>
2020-11-20 16:13:02 -0500

    removed vendor service and updated pages to remove vendor

--- src/Routes/ProductDetailPage/ProductDetail.js
+++ src/Routes/ProductDetailPage/ProductDetail.js
@@ -4,24 +4,24 @@ import './ProductDetail.css'
 import { API_ENDPOINT } from '../../config'
 
 const defaultState = {
-    vendor: {}
+    items: {}
 }
  
 export default function ProductDetail() {
     const [state, setState] = useState(defaultState);
     let { id } = useParams();
 
-    const getVendorProduct = () => {
+    const getItem = () => {
         fetch(`${API_ENDPOINT}/items/${id}`)
             .then(res => res.json())
-            .then(vendor => {
-                console.log({ vendor })
-                setState({ vendor })
+            .then(item => {
+                console.log({ item })
+                setState({ item })
             })
     }
 
     useEffect(() => {
-        getVendorProduct()
+        getItem()
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [id]);
 
@@ -34,21 +34,21 @@ export default function ProductDetail() {
 
                 <div className="product-detail-item-name">
                     <h5>
-                        {state.vendor.item_name}
+                        {state.items.name}
                     </h5>
                 </div>
 
                 <div className="product-detail-item-price-container">
                     <div className="product-detail-item-price tag">
-                        <p>${state.vendor.item_price}</p>
+                        <p>${state.items.itemPrice}</p>
                     </div>
                     <div className="product-detail-item-availability tag">
-                        <p>Available: {state.vendor.item_count}</p>
+                        <p>Available: {state.items.itemCount}</p>
                     </div>
                 </div>
             </div>
             <div className="product-detail-item-description">
-                <p>{state.vendor.item_description}</p>
+                <p>{state.items.description}</p>
             </div>
         </div>
     )
